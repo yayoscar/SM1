5afcb7b3a52eccab3b7c4cb5d0116a37d24c00c5
JadeColli <jade7385.jrcv@gmail.com>
2024-10-02 14:44:59 -0500

    entrega de tarea

---
+++ Descargando Datos/4. Índices Automáticos/4_Índices_Automáticos.py
@@ -0,0 +1,92 @@
+"""En esta sección, escribimos de manera fija los índices correspondientes a las columnas `TMIN` y `TMAX`.
+ Usa la fila de encabezado para determinar los índices de estos valores, de modo que tu programa pueda 
+ funcionar tanto para Sitka como para Death Valley.Usa el nombre de la estación para generar automáticamente
+ un título apropiado para tu gráfico."""
+
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+nombre_archivo = 'Descargando Datos/data0/death_valley_2021_simple.csv'
+nombre_archivo_1='Descargando Datos/data0/sitka_weather_2021_simple.csv'
+
+with open(nombre_archivo) as archivo:
+    lector = csv.reader(archivo)
+    fila_encabezado = next(lector)
+
+    # Obtener índices de TMIN y TMAX
+    indice_tmin = fila_encabezado.index('TMIN')
+    indice_tmax = fila_encabezado.index('TMAX')
+    indice_fecha = fila_encabezado.index('DATE')
+
+    # Obtener las fechas, y las temperaturas máximas y mínimas
+    altas, fechas, bajas = [], [], []
+    for fila in lector:
+        fecha_actual = datetime.strptime(fila[2], '%Y-%m-%d')
+        try:
+            alta = int(fila[3])
+            baja = int(fila[4])
+        except ValueError:
+            print(f"Datos faltantes para {fecha_actual}")
+        else:
+            fechas.append(fecha_actual)
+            altas.append(alta)
+            bajas.append(baja)
+
+with open(nombre_archivo_1) as archivo:
+    lector = csv.reader(archivo)
+    fila_encabezado = next(lector)
+
+    # Obtener índices de TMIN y TMAX
+    indice_tmin = fila_encabezado.index('TMIN')
+    indice_tmax = fila_encabezado.index('TMAX')
+    indice_fecha = fila_encabezado.index('DATE')
+
+    # Obtener las fechas, y las temperaturas máximas y mínimas
+    altas, fechas, bajas = [], [], []
+    for fila in lector:
+        fecha_actual = datetime.strptime(fila[2], '%Y-%m-%d')
+        try:
+            alta = int(fila[4])
+            baja = int(fila[5])
+        except ValueError:
+            print(f"Datos faltantes para {fecha_actual}")
+        else:
+            fechas.append(fecha_actual)
+            altas.append(alta)
+            bajas.append(baja)
+
+# Crear gráfico
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+
+ax.plot(fechas, altas, c='orange', alpha=0.5)
+ax.plot(fechas, bajas, c='yellow', alpha=0.5)
+plt.fill_between(fechas, altas, bajas, facecolor='blue', alpha=0.1)
+
+# Formatear gráfico
+#condicionales para los del death
+if nombre_archivo.endswith('death_valley_2021_simple.csv'):
+    titulo_death = f'Temperaturas máximas y mínimas año 2021\nDeath Valley, CA'
+elif nombre_archivo.endswith('sitka_weather_2021_simple.csv'):
+    titulo_death = f'Temperaturas máximas y mínimas año 2021\nSitka, AK'
+else:
+    nombre_lugar = nombre_archivo.split('/')[-1].split('_')[0].title()
+    titulo_death = f'Temperaturas máximas y mínimas año 2021\n{nombre_lugar}'
+
+#condicionales para los del sitka
+if nombre_archivo_1.endswith('sitka_weather_2021_simple.csv'):
+    titulo_sitka = f'Temperaturas máximas y mínimas año 2021\nSitka, AK,'
+elif nombre_archivo_1.endswith('death_valley_2021_simple.csv'):
+    titulo_sitka = f'Temperaturas máximas y mínimas año 2021\nDeath Valley, CA'
+else:
+    nombre_lugar_2 = nombre_archivo_1.split('/')[-1].split('_')[0].title()
+    titulo_sitka = f'Temperaturas máximas y mínimas del 2021\n{nombre_lugar_2}'
+
+plt.title(titulo_death, fontsize=16)
+plt.title(titulo_sitka, fontsize=16)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperatura', fontsize=16)
+fig.autofmt_xdate()
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
