bc5d0855716bb9bc5b8e76b457979c492fd52bbd
Oscar Perez Olan <operez@customsoft.biz>
2024-10-04 07:52:58 -0500

    Limpieza Main

--- Visualizando Datos/8. Multiplicación/Dado.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-class Dado:
-    def __init__(self,caras = 6):
-        self.caras = caras
-
-    def lanzar(self):
-        return randint(1,self.caras)
--- dado.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-class Dado:
-    def __init__(self, lados=6):
-        self.lados=lados
-    
-    def lanzar(self):
-        return randint(1, self.lados)
-    
